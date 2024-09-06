87ac66a83097cd512528e01d920ee81cf24f94d8
Hazel F Nguyen <hnguyen5@brynmawr.edu>
2024-09-06 15:58:46 -0400

    Finished HelloUnity

--- HelloUnity/Assets/Scripts/HelloUnity.cs
+++ HelloUnity/Assets/Scripts/HelloUnity.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,12 @@ public class HelloUnity : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello Unity");
+        Debug.Log("Hello Unity!!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       Debug.Log(Time.realtimeSinceStartup);
     }
 }
